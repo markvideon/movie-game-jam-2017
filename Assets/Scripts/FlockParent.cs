b41d4c95d8b44b31ac9e96891e5141bf962dd17d
Mark Videon <mark.videon@gmail.com>
2018-11-26 19:54:45 +1030

    ... Shamefully adding FlockParent to staging area

---
+++ Assets/Scripts/FlockParent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlockParent : MonoBehaviour
+{
+    private Vector3[] flockVelocities;
+    // Use this for initialization
+    void Start()
+    {
+        flockVelocities = new Vector3[this.transform.childCount];
+
+        for (int i = 0; i < this.transform.childCount; i++) {
+            flockVelocities[i] = this.transform.GetChild(i).GetComponent<Rigidbody>().velocity;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            flockVelocities[i] = this.transform.GetChild(i).GetComponent<Rigidbody>().velocity;
+        }
+    }
+
+    public Vector3[] getFlockVelocities() {
+        return flockVelocities;
+    }
+}
